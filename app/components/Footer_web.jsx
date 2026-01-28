f77ab1b8cd6d3b0d830aaf0f07dfe2dffec8df7d
hustmedia <dangtinn306@gmail.com>
2026-01-28 16:25:23 +0700

    sadadsdassadsaddsa

--- app/components/Footer_web.jsx
+++ app/components/Footer_web.jsx
@@ -1,9 +1,9 @@
 export default function FooterWeb() {
   return (
     <footer className="mt-auto w-full bg-slate-950/40 bg-hust-glass backdrop-blur-md border-t border-white/10 text-white py-0.5 md:py-0.5 text-xs md:text-base leading-none flex-none">
-      <div className=" mx-auto px-[12px] py-[16px] max-[480px]:px-2 max-[480px]:py-2 flex flex-col md:flex-row justify-between items-center gap-1 md:gap-1 text-[13px] md:text-[15px]">
+      <div className=" mx-auto px-[12px] py-[16px] max-[480px]:px-2 max-[480px]:py-[12px] flex flex-col md:flex-row justify-between items-center gap-1 md:gap-1 text-[13px] md:text-[15px]">
         {/* 1. Chính sách (Bên trái PC / Giữa Mobile) */}
-        <div className="flex gap-5 text-white w-full md:w-auto justify-center md:justify-start md:flex-1 order-1 md:order-1 font-medium">
+        <div className="flex flex-row gap-16 sm:gap-5 text-white w-full md:w-auto justify-center md:justify-start md:flex-1 order-1 md:order-1 font-medium">
           <a
             href="/next/info/terms_service"
             className="group hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.45)] transition-all duration-200"
